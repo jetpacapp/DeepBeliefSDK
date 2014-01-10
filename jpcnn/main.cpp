e074b9f68741e1e109b5df954d6c3199905ac7a4
Pete Warden <pete@jetpac.com>
2014-01-09 18:06:36 -0800

    Initial Commit

---
+++ jpcnn/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jpcnn
+//
+//  Created by Peter Warden on 1/9/14.
+//  Copyright (c) 2014 Jetpac, Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
